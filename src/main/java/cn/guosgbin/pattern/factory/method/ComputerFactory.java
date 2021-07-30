2b9552bebc11cd67fa0a3e38f82fe8a3e7de5475
Dylan Kwok <guosgbin@163.com>
2021-07-30 09:06:39 +0800

    工厂方法模式

---
+++ src/main/java/cn/guosgbin/pattern/factory/method/ComputerFactory.java
@@ -0,0 +1,18 @@
+package cn.guosgbin.pattern.factory.method;
+
+/**
+ * @author: Dylan kwok GSGB
+ * @date: 2021/7/30 9:00
+ * <p>
+ * 古之立大事者，不惟有超世之才，亦必有坚忍不拔之志——苏轼
+ */
+public class ComputerFactory {
+    public static Computer getComputer(String type, String ram, String hdd, String cpu) {
+        if ("PC".equalsIgnoreCase(type)) {
+            return new PC(ram, hdd, cpu);
+        } else if ("Server".equalsIgnoreCase(type)) {
+            return new Server(ram, hdd, cpu);
+        }
+        return null;
+    }
+}
